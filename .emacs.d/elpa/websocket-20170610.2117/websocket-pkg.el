eb76e6719634cbac785a25f5fd289b42695a1f6d
John Waczak <waczakj@oregonstate.edu>
2017-07-10 08:54:17 -0700

    added init file (emacs)

---
+++ .emacs.d/elpa/websocket-20170610.2117/websocket-pkg.el
@@ -0,0 +1,1 @@
+(define-package "websocket" "20170610.2117" "Emacs WebSocket client and server" 'nil :commit "a9b8e74fa190024cd450ef4f832482e8438674fa" :keywords '("communication" "websocket" "server"))
